c7b8edcc897a0cf1cd9557cc632d4029c844d406
MosaabGhaley <mosaab.ghaley@gmail.com>
2024-05-05 15:54:22 +0300

    Frontend home page done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,11 @@
 // App.js
 import "./App.css";
-import Header from "./Pages/Components/Shared/header/Header.js";
-import Navbar from "./Pages/Components/Shared/navbar/Navbar.js";
-import FooterSection from "./Pages/Components/Shared/FooterSection/FooterSection.js";
-import MainSection from "./Pages/Components/MainPage/MainSection/MainSection.js";
-import SaleSection from "./Pages/Components/MainPage/SaleSection/SaleSection.js";
-import BlockItemsGroup from "./Pages/Components/MainPage/BlockItemsGroup/BlockItemsGroup.js";
+import HomePage from "./Pages/HomePage.js";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Navbar />
-      <MainSection />
-      <SaleSection />
-      <BlockItemsGroup />
-      <FooterSection />
+      <HomePage />
       {/* Other page content goes here */}
     </div>
   );
