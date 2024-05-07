809bb82736a9681a24e755c6716d4e39fe7aa611
amira1m <20201700150@cis.asu.edu.eg>
2024-05-07 17:07:55 +0300

    commit after changes

--- order/src/main/java/com/order/controllers/OrderController.java
+++ order/src/main/java/com/order/controllers/OrderController.java
@@ -1,4 +1,49 @@
 package com.order.controllers;
 
+import com.order.models.Order;
+import com.order.models.OrderProduct;
+import com.order.models.OrderView;
+import com.order.servisces.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Time;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/orders")
 public class OrderController {
+
+private final OrderService orderService;
+
+    @GetMapping("")
+    public ResponseEntity<List<OrderView>> getAll(){
+        List<OrderView>products=this.orderService.getorders();
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable int id) {
+       orderService.deleteorder(id);
+        return new ResponseEntity<>("Deleted Successfully!", HttpStatus.OK);
+    }
+
+    @PostMapping("/create-with-products")
+    public ResponseEntity<Order> createOrderWithProducts(
+            @RequestParam UUID user_id,
+            @RequestParam Double total_price,
+            @RequestParam Time created_at,
+            @RequestBody List<OrderProduct> orderProducts
+    ) {
+        // Create the Order and associated OrderProduct records
+        Order createdOrder = orderService.createOrderWithProducts(
+              user_id, total_price, created_at, orderProducts);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdOrder); // Return the created Order
+    }
+
 }
---
+++ order/src/main/java/com/order/servisces/OrderService.java
@@ -0,0 +1,46 @@
+package com.order.servisces;
+
+import com.order.models.Order;
+import com.order.models.OrderProduct;
+import com.order.models.OrderView;
+import com.order.repositories.OrderProductRepository;
+import com.order.repositories.OrderRepository;
+import com.order.repositories.OrderViewRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.sql.Time;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private  OrderRepository orderRepository;
+    private OrderProductRepository orderProductRepository;
+    private OrderViewRepository orderViewRepository;
+
+    public List<OrderView> getorders(){
+        return this.orderViewRepository.findAll();
+    }
+
+    public void deleteorder(Integer id) {
+        this.orderRepository.deleteById(id);
+    }
+
+public Order createOrderWithProducts(UUID user_id, Double total_price, Time created_at, List<OrderProduct> orderProducts) {
+        Order newOrder = Order.builder()
+                .user_id(user_id)
+                .total_price(total_price)
+                .created_at(created_at)
+                .build();
+
+        Order savedOrder = orderRepository.save(newOrder);
+        for (OrderProduct op : orderProducts) {
+            op.setOrder_id(savedOrder.getId());
+            orderProductRepository.save(op);
+        }
+        return savedOrder;
+    }
+}
