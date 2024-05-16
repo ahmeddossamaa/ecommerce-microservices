38e4ea17f08f40f3859910facd130df8c770d1ad
Ahmed Osama <70448761+ahmeddossamaa@users.noreply.github.com>
2024-05-16 04:27:09 +0300

    updates

--- product/src/main/java/com/product/controllers/ProductController.java
+++ product/src/main/java/com/product/controllers/ProductController.java
@@ -4,10 +4,8 @@ import com.product.dto.ProductRequestDTO;
 import com.product.models.Product;
 import com.product.services.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
